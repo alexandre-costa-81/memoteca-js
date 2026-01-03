3579418a2c68fb1e547c82c84ae3cf7eebef1fa0
Alexandre Costa <alexandre.costa@furg.br>
2026-01-03 11:11:29 -0300

    adiciona função para manipular submissão de formulário e salvar pensamentos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos();
+
+  const formularioPensamento = document.getElementById("pensamento-form");
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
 });
+
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault();
+
+  const id = document.getElementById("pensamento-id").value;
+  const conteudo = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autoria").value;
+
+  const pensamento = { conteudo, autoria };
+
+  try {
+    await api.salvarPensamento(pensamento);
+    ui.renderizarPensamentos();
+  } catch (error) {
+    console.error("Erro ao salvar pensamento: ", error);
+  }
+}
